e30b7938d39379adf30ae895ef6e4850c0c4602e
Alex Paulson <whileforkdofork@gmail.com>
2014-05-30 17:30:01 +0100

    Added intern.h

---
+++ intern.h
@@ -0,0 +1,41 @@
+/*
+ * Various input/output functions
+ *
+ * @(#)io.c	9.0	(rdk)	 7/17/84
+ *
+ * Super-Rogue
+ * Copyright (C) 1984 Robert D. Kindelberger
+ * All rights reserved.
+ *
+ * Based on "Rogue: Exploring the Dungeons of Doom"
+ * Copyright (C) 1980, 1981 Michael Toy, Ken Arnold and Glenn Wichman
+ * All rights reserved.
+ *
+ * See the file LICENSE.TXT for full copyright and licensing information.
+ */
+
+#include <stdarg.h>
+#include <ctype.h>
+#include <string.h>
+/* #include "rogue.h" */
+/* #include "rogue.ext" */
+
+/*
+ * msg:
+ *	Display a message at the top of the screen.
+ */
+static char msgbuf[BUFSIZ];
+static int newpos = 0;
+
+int msg(char *fmt, ...);
+int addmsg(char *fmt, ...);
+int endmsg();
+int doadd(char *fmt, va_list ap);
+int step_ok();
+int dead_end();
+int waste_time();
+int setoflg();
+int dropcheck();
+int o_on();
+int cansee();
+int do_chase();
